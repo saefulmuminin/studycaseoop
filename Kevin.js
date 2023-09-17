ec08482b15fdefea8fe5e6ca22e219f6010b0e86
Kevin Pandoh <kevinmpandoh@gmail.com>
2023-09-17 14:57:49 +0800

    first commit

---
+++ Kevin.js
@@ -0,0 +1,25 @@
+class BangunDatar {
+    constructor() {
+        this.pi = 3.14
+    }
+
+    luasPersegi(sisi){
+        return sisi * sisi
+    }
+
+    luasPersegiPanjang(panjang, lebar) {
+        return panjang * lebar
+    }
+
+    luasLingkaran(jariJari) {
+        return this.pi * jariJari * jariJari
+    }
+
+    luasSegitiga(alas, tinggi) {
+        return 1/2 * (alas * tinggi)
+    }
+
+    luasTrapesium(a, b, tinggi) {
+        return 1/2 * (a + b) * tinggi
+    }
+}
