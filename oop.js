fe7b21202e00942c29871f45d157139e9907a5f3
Saeful mu'minin <saeful2026027@gmail.com>
2023-09-17 14:31:26 +0700

    comiit

--- oop.js
+++ oop.js
@@ -1,77 +1,75 @@
-class Persegi {
-    constructor(sisi) {
-      this.sisi = sisi;
-    }
-  
-    hitungLuas() {
-      return this.sisi * this.sisi;
-    }
-  
-    hitungKeliling() {
-      return 4 * this.sisi;
-    }
+class BangunDatar {
+  constructor(nama) {
+    this.nama = nama;
   }
-  class PersegiPanjang {
-    constructor(panjang, lebar) {
-      this.panjang = panjang;
-      this.lebar = lebar;
-    }
-  
-    hitungLuas() {
-      return this.panjang * this.lebar;
-    }
-  
-    hitungKeliling() {
-      return 2 * (this.panjang + this.lebar);
-    }
+
+  namaBangunDatar() {
+    return "Bangun Datar: " + this.nama;
   }
-  class Lingkaran {
-    constructor(jariJari) {
-      this.jariJari = jariJari;
-    }
-  
-    hitungLuas() {
-      return Math.PI * Math.pow(this.jariJari, 2);
-    }
-  
-    hitungKeliling() {
-      return 2 * Math.PI * this.jariJari;
-    }
+
+  hitungLuas() {}
+
+  hitungKeliling() {}
+}
+
+class Persegi extends BangunDatar {
+  constructor(sisi) {
+    super("persegi");
+    this.sisi = sisi;
   }
-  class Segitiga {
-    constructor(alas, tinggi) {
-      this.alas = alas;
-      this.tinggi = tinggi;
-    }
-  
-    hitungLuas() {
-      return 0.5 * this.alas * this.tinggi;
-    }
-  
-    // Untuk menghitung keliling segitiga, Anda perlu mengetahui panjang ketiga sisi,
-    // karena sisi segitiga bisa bervariasi.
-    // Jadi, method hitungKeliling() sebaiknya membutuhkan parameter tambahan.
-    hitungKeliling(sisi1, sisi2, sisi3) {
-      return sisi1 + sisi2 + sisi3;
-    }
+
+  hitungLuas() {
+    return this.sisi * this.sisi;
   }
-  class Trapesium {
-    constructor(sisiAtas, sisiBawah, tinggi, sisiMiring) {
-      this.sisiAtas = sisiAtas;
-      this.sisiBawah = sisiBawah;
-      this.tinggi = tinggi;
-      this.sisiMiring = sisiMiring;
-    }
-  
-    hitungLuas() {
-      return ((this.sisiAtas + this.sisiBawah) / 2) * this.tinggi;
-    }
-  
-    // Untuk menghitung keliling trapesium, Anda perlu mengetahui panjang keempat sisi.
-    // Jadi, method hitungKeliling() sebaiknya membutuhkan parameter tambahan.
-    hitungKeliling(sisi1, sisi2, sisi3, sisi4) {
-      return sisi1 + sisi2 + sisi3 + sisi4;
-    }
+
+  hitungKeliling() {
+    return 4 * this.sisi;
   }
-    
-      
+}
+
+class PersegiPanjang extends BangunDatar {
+  constructor(panjang, lebar) {
+    super("persegi Panjang: ");
+    this.panjang = panjang;
+    this.lebar = lebar;
+  }
+
+  hitungLuas() {
+    return this.panjang * this.lebar;
+  }
+
+  hitungKeliling() {
+    return 2 * (this.panjang + this.lebar);
+  }
+}
+
+class Lingkaran extends BangunDatar {
+  constructor(jariJari) {
+    super("Lingkaran");
+    this.jariJari = jariJari;
+  }
+
+  hitungLuas() {
+    return Math.PI * this.jariJari * this.jariJari;
+  }
+
+  hitungKeliling() {
+    return 2 * Math.PI * this.jariJari;
+  }
+}
+
+const persegi1 = new Persegi(5);
+const persegiPanjang1 = new PersegiPanjang(5, 3);
+const lingkaran1 = new Lingkaran(7);
+
+console.log(persegi1.namaBangunDatar());
+console.log("Luas:", persegi1.hitungLuas());
+console.log("Keliling:", persegi1.hitungKeliling());
+
+console.log(persegiPanjang1.namaBangunDatar());
+console.log("Luas:", persegiPanjang1.hitungLuas());
+console.log("Keliling:", persegiPanjang1.hitungKeliling());
+
+console.log(lingkaran1.namaBangunDatar());
+console.log("Luas:", lingkaran1.hitungLuas());
+console.log("Keliling:", lingkaran1.hitungKeliling());
